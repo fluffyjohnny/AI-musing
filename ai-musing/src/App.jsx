c4aa20df3c2238ea9850186fa64eaabafd9874ab
John Chia <john.chia0106@gmail.com>
2022-05-19 18:03:02 -0400

    init commit

---
+++ ai-musing/src/App.jsx
@@ -0,0 +1,76 @@
+import logo from "./logo.svg";
+import "./App.css";
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  Container,
+  Button,
+  Tabs,
+  styled
+} from "@mui/material";
+import SmartToyIcon from "@mui/icons-material/SmartToy";
+
+const sx = {
+  mr: 1,
+  color: "white",
+  display: "block",
+  letterSpacing: ".2em",
+  fontFamily: "monospace",
+  align: "left",
+  fontWeight: 500,
+};
+
+const BootstrapButton = styled(Button)({
+  boxShadow: '5px, 10px',
+  textTransform: 'none',
+  fontSize: 12,
+  padding: '6px 12px',
+  '&:hover': {
+    transform: 'translateY(-0.25em)',
+  },
+  '&:active': {
+    boxShadow: 'none',
+    backgroundColor: '#4287f5',
+    borderColor: '#005cbf',
+  },
+});
+
+function App() {
+  return (
+    <div className="App">
+      <AppBar position="relative">
+        <Container maxWidth="xl">
+          <Toolbar disableGutters>
+            <SmartToyIcon sx={{ mr: 3 }} />
+            <Typography
+              variant="h7"
+              noWrap
+              sx={{
+                display: { xs: "none", md: "flex" },
+                fontFamily: "monospace",
+                fontWeight: 800,
+                letterSpacing: ".4rem",
+                color: "inherit",
+                textDecoration: "none",
+              }}
+            >
+              {" "}
+              AI-musing
+            </Typography>
+            <Tabs sx={{ marginLeft: "auto" }} />
+
+            <BootstrapButton key="1" href='https://beta.openai.com/docs/guides/completion' size="medium" sx={sx}>
+              About This API
+            </BootstrapButton>
+            <BootstrapButton key="2" size="medium" sx={sx}>
+              Author
+            </BootstrapButton>
+          </Toolbar>
+        </Container>
+      </AppBar>
+    </div>
+  );
+}
+
+export default App;
